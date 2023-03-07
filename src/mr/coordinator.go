569c744e4078b2865f4294cdc17d74f77575465b
“baojinchang <intersder@outlook.com>
2023-03-07 22:46:06 +0800

    mapreduce

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,15 +1,19 @@
 package mr
 
-import "log"
+import (
+	"log"
+	"time"
+)
 import "net"
 import "os"
 import "net/rpc"
 import "net/http"
 
-
 type Coordinator struct {
-	// Your definitions here.
-
+	mapWait    chan TaskInfo
+	mapRun     chan TaskInfo
+	reduceWait chan TaskInfo
+	reduceRun  chan TaskInfo
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -19,11 +23,61 @@ type Coordinator struct {
 //
 // the RPC argument and reply types are defined in rpc.go.
 //
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
+func (c *Coordinator) RequestTask(args *TaskInfo, reply *TaskInfo) error {
+	if len(c.mapWait) != 0 {
+		*reply = <-c.mapWait
+		reply.Status = "map"
+		reply.Time = GetNowTime()
+		c.mapRun <- *reply
+
+		return nil
+	}
+	if len(c.mapRun) == 0 && len(c.mapWait) == 0 && len(c.reduceWait) != 0 {
+		*reply = <-c.reduceWait
+		reply.Status = "reduce"
+		reply.Time = GetNowTime()
+		c.reduceRun <- *reply
+		return nil
+	}
+	reply.Status = "wait"
+
 	return nil
 }
 
+func (c *Coordinator) TaskDone(args *TaskInfo, reply *TaskInfo) error {
+	if args.Status == "map" {
+		for {
+			select {
+			case item := <-c.mapRun:
+				{
+					if args.FileIndex != item.FileIndex {
+						c.mapRun <- item
+					}
+					if args.FileIndex == item.FileIndex {
+						return nil
+					}
+				}
+			}
+		}
+	}
+	if args.Status == "reduce" {
+		for {
+			select {
+			case item := <-c.reduceRun:
+				{
+					if args.ReduceIndex != item.ReduceIndex {
+						c.reduceRun <- item
+					}
+					if args.ReduceIndex == item.ReduceIndex {
+						return nil
+					}
+				}
+			}
+		}
+	}
+
+	return nil
+}
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -41,6 +95,36 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
+func (c *Coordinator) RemoveTimeOutTask() {
+	for {
+		time.Sleep(time.Second)
+		if len(c.mapRun) != 0 {
+			select {
+			case item := <-c.mapRun:
+				{
+					if GetNowTime()-item.Time < 10 {
+						c.mapRun <- item
+					} else {
+						c.mapWait <- item
+					}
+				}
+			}
+		}
+		if len(c.reduceRun) != 0 {
+			select {
+			case item := <-c.reduceRun:
+				{
+					if GetNowTime()-item.Time < 10 {
+						c.reduceRun <- item
+					} else {
+						c.reduceWait <- item
+					}
+				}
+			}
+		}
+	}
+}
+
 //
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
@@ -48,8 +132,9 @@ func (c *Coordinator) server() {
 func (c *Coordinator) Done() bool {
 	ret := false
 
-	// Your code here.
-
+	if len(c.mapWait) == 0 && len(c.mapRun) == 0 && len(c.reduceRun) == 0 && len(c.reduceWait) == 0 {
+		return true
+	}
 
 	return ret
 }
@@ -60,11 +145,20 @@ func (c *Coordinator) Done() bool {
 // nReduce is the number of reduce tasks to use.
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
-
-	// Your code here.
-
-
+	c := Coordinator{make(chan TaskInfo, 10), make(chan TaskInfo, 10), make(chan TaskInfo, 10), make(chan TaskInfo, 10)}
+	for i := 0; i < len(files); i++ {
+		task := TaskInfo{files[i], i, "map", nReduce, 0}
+		c.mapWait <- task
+	}
+	for i := 0; i < nReduce; i++ {
+		reduce := TaskInfo{"aaa", len(files), "reduce", i + 1, 0}
+		c.reduceWait <- reduce
+	}
+	go c.RemoveTimeOutTask()
 	c.server()
 	return &c
+}
+
+func GetNowTime() int64 {
+	return time.Now().UnixNano() / int64(time.Second)
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,18 +1,40 @@
 package mr
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+)
 import "log"
 import "net/rpc"
 import "hash/fnv"
 
-
 //
 // Map functions return a slice of KeyValue.
 //
+type TaskInfo struct {
+	FileName    string
+	FileIndex   int
+	Status      string
+	ReduceIndex int
+	Time        int64
+}
+
 type KeyValue struct {
 	Key   string
 	Value string
 }
+
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
 //
 // use ihash(key) % NReduce to choose the reduce
@@ -24,18 +46,124 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
 //
 // main/mrworker.go calls this function.
 //
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
+	arg := TaskInfo{}
+	reply := TaskInfo{}
+	for true {
+		_ = call("Coordinator.RequestTask", &arg, &reply)
+		switch reply.Status {
+		case "map":
+			TaskMap(mapf, &reply)
+			break
+		case "reduce":
+			TaskReduce(reducef, &reply)
+			break
+		case "wait":
+			time.Sleep(time.Second)
+			break
+		case "":
+			fmt.Printf("Nothing")
+			return
+		}
+	}
+	return
+}
 
-	// Your worker implementation here.
+func TaskMap(mapf func(string, string) []KeyValue, reply *TaskInfo) {
+	file, err := os.Open(reply.FileName)
+	if err != nil {
+		log.Fatalf("cannot open %v", reply.FileName)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", reply.FileName)
+	}
+	file.Close()
+	kva := mapf(reply.FileName, string(content))
+	kvas := make([][]KeyValue, reply.ReduceIndex)
+	for i := 0; i < reply.ReduceIndex; i++ {
+		kvas[i] = make([]KeyValue, 0)
+	}
+	for _, item := range kva {
+		index := ihash(item.Key) % reply.ReduceIndex
+		kvas[index] = append(kvas[index], item)
+	}
+	for i := 0; i < reply.ReduceIndex; i++ {
+		tempfile, err := os.CreateTemp(".", "mrtemp")
+		if err != nil {
+			fmt.Println("error")
+		}
+		enc := json.NewEncoder(tempfile)
+		for _, kv := range kvas[i] {
+			err := enc.Encode(&kv)
+			if err != nil {
+				fmt.Println("Json encode failed")
+			}
+		}
+		outname := fmt.Sprintf("mr-%v-%v", reply.FileIndex, i)
+		err = os.Rename(tempfile.Name(), outname)
+		if err != nil {
+			fmt.Println("error")
+		}
+	}
+	task := TaskInfo{}
+	call("Coordinator.TaskDone", &reply, &task)
+	return
+}
 
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+func TaskReduce(reducef func(string, []string) string, reply *TaskInfo) {
+	reply.ReduceIndex -= 1
+	intermediate := []KeyValue{}
+	for i := 0; i < reply.FileIndex; i++ {
+		fileName := "mr-"
+		fileName += strconv.Itoa(i) + "-" + strconv.Itoa(reply.ReduceIndex)
+		file, err := os.Open(fileName)
+		if err != nil {
+			log.Fatalf("cannot open %v", reply.FileName)
+		}
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			intermediate = append(intermediate, kv)
+		}
+		file.Close()
+	}
+	sort.Sort(ByKey(intermediate))
+	tempfile, err := os.CreateTemp(".", "mrtemp")
+	if err != nil {
+		fmt.Println("error")
+	}
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
 
+		// this is the correct format for each line of Reduce output.
+		fmt.Fprintf(tempfile, "%v %v\n", intermediate[i].Key, output)
+		i = j
+	}
+	outname := fmt.Sprintf("mr-out-%v", reply.ReduceIndex)
+	err = os.Rename(tempfile.Name(), outname)
+	if err != nil {
+		fmt.Println("error")
+	}
+	task := TaskInfo{}
+	call("Coordinator.TaskDone", &reply, &task)
+	return
 }
 
 //
@@ -43,29 +171,6 @@ func Worker(mapf func(string, string) []KeyValue,
 //
 // the RPC argument and reply types are defined in rpc.go.
 //
-func CallExample() {
-
-	// declare an argument structure.
-	args := ExampleArgs{}
-
-	// fill in the argument(s).
-	args.X = 99
-
-	// declare a reply structure.
-	reply := ExampleReply{}
-
-	// send the RPC request, wait for the reply.
-	// the "Coordinator.Example" tells the
-	// receiving server that we'd like to call
-	// the Example() method of struct Coordinator.
-	ok := call("Coordinator.Example", &args, &reply)
-	if ok {
-		// reply.Y should be 100.
-		fmt.Printf("reply.Y %v\n", reply.Y)
-	} else {
-		fmt.Printf("call failed!\n")
-	}
-}
 
 //
 // send an RPC request to the coordinator, wait for the response.
